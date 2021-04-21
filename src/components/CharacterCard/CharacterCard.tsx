6faded5098bc14e4d92e1870398e149a665c5a21
Sacha Lifszyc <sacha.lifszyc@toptal.com>
2021-04-20 21:29:24 -0300

    Agregar Content de Morty

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
+import { Typography } from '../Typography'
 
-import { Card, Image, ImageWrapper } from './styles'
+import { Card, ContentWrapper, Image, ImageWrapper, Section } from './styles'
 
 const CharacterCard: FC = () => (
   <Card>
@@ -10,6 +11,32 @@ const CharacterCard: FC = () => (
         alt='Morty Smith'
       />
     </ImageWrapper>
+    <ContentWrapper>
+      <Section>
+        <Typography variant='heading' color='white' size='big'>
+          Morty Smith
+        </Typography>
+        <Typography color='white' size='small'>
+          Alive - Human
+        </Typography>
+      </Section>
+      <Section>
+        <Typography color='grey' size='small'>
+          Última ubicación conocida:
+        </Typography>
+        <Typography color='white' size='medium'>
+          Story Train
+        </Typography>
+      </Section>
+      <Section>
+        <Typography color='grey' size='small'>
+          Primer episodio donde aparece:
+        </Typography>
+        <Typography color='white' size='medium'>
+          Never Ricking Morty
+        </Typography>
+      </Section>
+    </ContentWrapper>
   </Card>
 )
 
